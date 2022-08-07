0356c22cbd3088953c1a0d95cce00050ee07e898
rajesh.kumar@goavega.com-at-012114828737 <omAQZCpcWPwd4hRX5oJ8PtZh9+6+lM1W3mI5Gv6pHng=>
2022-08-07 21:32:57 +0530

    templates added!

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,51 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/user.router');
+const hbs = require('hbs');
 
 require('./db/mongoose.ts');
 
 dotenv.config(); //dotenv configed
 const app = express();
+const pathFile = path.join(__dirname, './server.ts');
+const viewPath = path.join(__dirname, '../src/templates/views');
+const partialPath = path.join(__dirname, '../src/templates/partials');
 const port = process.env.PORT || 3000;
+
+// View Engine Setup
+app.set('views', path.join(__dirname));
+app.set('view engine', 'hbs');
 
 app.use(express.json()); // allowing express app to send json
 app.use(express.urlencoded({ extended: true })); //express defaulting haivng this bodyparser things to handle post req
 
+app.set('view engine', 'hbs'); //hbs setup //Dynamic web pages
+app.set('views', viewPath); //view path
+hbs.registerPartials(partialPath); //partials path
+
 // Home Route
 app.get('/', (req, res) => {
-  res.send('Hello Node!');
+  res.render('index', {
+    title: 'This is the home page for node CURD',
+    author: 'Rajesh',
+  });
+});
+
+//about
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Page',
+    author: 'Mahesh',
+  });
+});
+
+//help
+app.get('/help', (req, res) => {
+  res.render('about', {
+    title: 'Help Page',
+    author: 'Jay',
+  });
 });
 
 //other routes
